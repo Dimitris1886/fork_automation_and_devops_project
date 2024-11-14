9613a729d37ed0422209fa6214aed16883fd56a9
Dimitris1886 <dimaklimenko90@gmail.com>
2024-11-14 19:46:37 +0100

    Updated pipeline

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-import os
+import os, logging
 
 from flask import Flask, render_template, request
 import requests
@@ -9,6 +9,8 @@ app = Flask(__name__)
 
 API_URL = 'https://api.freecurrencyapi.com/v1/latest'
 API_KEY = os.getenv('FREE_CURRENCY_API_KEY')
+
+logging.info(f"The API key = {API_KEY}")
 
 CURRENCIES = [
     'USD', 'EUR', 'GBP', 'JPY', 'AUD', 'CAD', 'CHF', 'CNY',
